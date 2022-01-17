b3324963b570f7b15686c93a51a4e8a3a46c8991
Elliott Woodward <elliottcwoodward@gmail.com>
2022-01-17 13:13:17 -0500

    Nested comments built

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @commentable
+    else
+      redirect_to @commentale, alert: "Something went wrong"
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :parent_id)
+    end
+end
