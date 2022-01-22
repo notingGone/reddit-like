35224bad4680ce6be99ecd298a68846bc513a7dd
Elliott Woodward <elliottcwoodward@gmail.com>
2022-01-22 10:26:14 -0500

    Fix comments#delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  # before_action :set_comment, only: %i[ :destroy ]
 
   def create
     @comment = @commentable.comments.new(comment_params)
@@ -7,11 +8,26 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @commentable
     else
-      redirect_to @commentale, alert: "Something went wrong"
+      redirect_to @commentable, alert: "Something went wrong"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to post_path(@commentable), status: :see_other, notice: "comment was successfully destroyed." }
+      format.json { head :no_content }
     end
   end
 
   private
+
+    # def set_comment
+    #   @comment = Comment.find(params[:id])
+    # end
+
     def comment_params
       params.require(:comment).permit(:body, :parent_id)
     end
--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -3,6 +3,7 @@ class Posts::CommentsController < CommentsController
 
   private
     def set_commentable
+      # debugger
       @commentable = Post.find(params[:post_id])
     end
 end
